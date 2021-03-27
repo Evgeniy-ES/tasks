9d77eb646958d1914c03080db6bff5ab7dd23e26
Evgeniy <ees86@yandex.ru>
2021-03-27 22:56:55 +0300

    Part2

---
+++ work/Part1/exp2.rb
@@ -0,0 +1,9 @@
+print "Enter triangle base "
+base = gets.chomp.to_i
+
+print "Enter triangle height "
+height = gets.chomp.to_i
+
+area =  0.5 * base * height
+
+puts "Area triangle = #{area}"
---
+++ work/Part1/exp4.rb
@@ -0,0 +1,21 @@
+print "We have a quadratic equation of the form  a*x*x + b*x + c = 0"
+puts
+print "Enter koefficient a = "
+a = gets.chomp.to_i
+
+print "Enter koefficient b = "
+b = gets.chomp.to_i
+
+print "Enter koefficient c = "
+c = gets.chomp.to_i
+
+discr = b*b - 4*a*c
+puts
+if discr < 0
+  puts "The quadratic equation has no roots"
+elsif discr == 0
+  puts "Root = #{-b/(2*a)}"
+else
+  puts "Root1 = #{(-b + Math.sqrt(discr))/(2*a)}"
+  puts "Root2 = #{(-b - Math.sqrt(discr))/(2*a)}"
+end
