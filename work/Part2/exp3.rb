f40e6efc5a65e3899fd0b854d308d4ba6700e503
Evgeniy <ees86@yandex.ru>
2021-03-29 10:40:37 +0300

    correct1

--- work/Part2/exp3.rb
+++ work/Part2/exp3.rb
@@ -1,32 +1,20 @@
-print "Enter the first side of the triangle  "
+print 'Enter the first side of the triangle  '
 first = gets.chomp.to_i
 
-print "Enter the second side of the triangle  "
+print 'Enter the second side of the triangle  '
 second = gets.chomp.to_i
 
-print "Enter the third side of the triangle  "
+print 'Enter the third side of the triangle  '
 third = gets.chomp.to_i
 
 if first == second && second == third
-  puts "isosceles triangle"
+  puts 'isosceles triangle'
 else
-  if first > second && first > third
-    max_side = first
-    side1 = second
-    side2 = third
-  elsif second > first && second > third
-    max_side = second
-    side1 = first
-    side2 = third
-  else
-    max_side = third
-    side1 = first
-    side2 = second
-  end
+  a, b, hypo = [first, second, third].sort
 
-  if max_side * max_side == side1 * side1 + side2 * side2
-    puts "the triangle is rectangular"
+  if hypo ** 2 == a ** 2 + b ** 2
+    puts 'the triangle is rectangular'
   else
-    puts "the triangle is not rectangular"
+    puts 'the triangle is not rectangular'
   end
 end
--- work/Part2/exp4.rb
+++ work/Part2/exp4.rb
@@ -1,18 +1,18 @@
-print "We have a quadratic equation of the form  a*x*x + b*x + c = 0"
+print 'We have a quadratic equation of the form  a*x*x + b*x + c = 0'
 puts
-print "Enter koefficient a = "
+print 'Enter koefficient a = '
 a = gets.chomp.to_i
 
-print "Enter koefficient b = "
+print 'Enter koefficient b = '
 b = gets.chomp.to_i
 
-print "Enter koefficient c = "
+print 'Enter koefficient c = '
 c = gets.chomp.to_i
 
 discr = b*b - 4*a*c
 puts
 if discr < 0
-  puts "The quadratic equation has no roots"
+  puts 'The quadratic equation has no roots'
 elsif discr == 0
   puts "Root = #{-b/(2*a)}"
 else
