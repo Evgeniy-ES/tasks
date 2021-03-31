284f7dfc8b2bc390e6a6c72919f845cb13154847
Evgeniy <ees86@yandex.ru>
2021-03-31 13:08:50 +0300

    Part3_correct

--- work/Part3/exp4.rb
+++ work/Part3/exp4.rb
@@ -3,10 +3,9 @@ letters= ('a'..'z')
 vowels_array = %w{ a e i o u y }
 vowels_hash = {}
 count = 1
-letters.each do |letter|
+letters.each_with_index do |letter, index|
   if vowels_array.include?(letter)
-    vowels_hash[letter] = count
+    vowels_hash[letter] = index
   end
-  count += 1
 end
 puts vowels_hash
