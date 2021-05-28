9d2b36ea04e763773ba6ee65e0ca9b9bc4da05d6
Evgeniy <ees86@yandex.ru>
2021-05-28 10:54:23 +0300

    Part10_correct_4

--- work/Part10/all_modules.rb
+++ work/Part10/all_modules.rb
@@ -7,12 +7,9 @@ module Accessors
   module ClassMethods
 
     def attr_accessor_with_history(*methods)
-      options = methods.last.is_a?(Hash)? methods.pop: {}
       methods.each do |method|
-        raise TypeError.new("method name is not symbol") unless method.is_a?(Symbol)
         define_method(method) do
-          instance_variable_get("@#{method}") ||
-          instance_variable_set("@#{method}", options[:default])
+          instance_variable_get("@#{method}") 
         end
 
       define_method("#{method}=") do |value|
